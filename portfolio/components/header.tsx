8acaf9f27216a1c95a43dd743cd9b815d8a7cc22
Chamsron <emmacameron219@gmail.com>
2024-02-12 15:57:07 +0000

    added framer motion to the header, as well as different links using nav and li

--- portfolio/components/header.tsx
+++ portfolio/components/header.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import {motion} from "framer-motion";
+import { links } from '@/lib/data';
+import Link from 'next/link';
 
 export default function Header() {
   return(
@@ -11,6 +13,22 @@ export default function Header() {
         animate={{y: 0, x: "-50%", opacity: 1}}
         >
         </motion.div>
+        <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+            <ul className=' flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-indigo-800 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+                {links.map(link => (
+                    <motion.li className='h-3/4 flex items-center justify-center'
+                    initial={{ y: -100, opacity: 0}}
+                    animate={{y: 0, opacity: 1}}
+                    key={link.hash}>
+                        <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-indigo-950 transition hover:font-extrabold'
+                        href={link.hash}>
+                        {link.name}
+                        </Link>
+                    </motion.li>
+                ))}
+
+            </ul>
+        </nav>
     </header>
   )
 }
