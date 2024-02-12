99b82397e466303848f4f1dbb8d7add803c1a125
Chamsron <emmacameron219@gmail.com>
2024-02-12 14:35:28 +0000

    created background and stying for header

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,12 +15,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='"scroll-smooth'>
+
       <body className={`${inter.className}
       bg-indigo-50 text-indigo-950 relative`}>
         <div className="bg-violet-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+
         <div className="bg-sky-100 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
+
+
         </body>
     </html>
   )
