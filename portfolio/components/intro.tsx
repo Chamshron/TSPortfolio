067d2b8f42d999b85e54496fa2bb9a6409b4123a
Chamsron <emmacameron219@gmail.com>
2024-02-14 12:24:24 +0000

    intro section completed with animation and links

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import Image from 'next/image';
 import {motion} from "framer-motion";
+import Link from 'next/link';
+import {BsArrowRight, BsLinkedin} from "react-icons/bs";
+import {HiDownload} from "react-icons/hi";
+import {FaGithubSquare} from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section className=''>
+    <section className=' mb-28 max-w-[50rem] text-center sm:mb-0'>
         <div className='flex items-center justify-center'>
             <motion.div
             initial={{ scale:0, opacity: 0}}
@@ -37,6 +41,40 @@ export default function Intro() {
                         </motion.span>
             </motion.div>
         </div>
+        <motion.p className='mb-10 mt-4 text-2xl font-medium leading-[1.5] sm:text-4xl'
+        initial={{opacity:0, y:100}}
+        animate={{opacity:1, y:0}}
+        >
+                <span className='font-bold'> {`Hi, I'm Emma.`}</span> {`I'm a`} {" "}
+                <span className='font-bold'>full-stack developer</span> with a background in CRM customer support. I enjoy buildings
+                <span className='italic'> sites & apps </span>.
+            </motion.p>
+            <motion.div 
+            className=' flex flex-col sm:flex-row  items-center justify-center gap-3 px-4 text-lg font-medium'
+            initial={{opacity:0, y:100}}
+            animate={{opacity:1, y:0}}
+            transition={{
+                delay:0.1,
+            }}
+            >
+                <Link href="#contact"
+                className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:bg-950 active:scale-105 transition'>
+                    Contact me here<BsArrowRight className=' opacity-70 group-hover:translate-x-1 transition' />{" "}</Link>
+                <a className=' group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10'
+                href='/EmmaCameron_Resume.pdf'
+                download
+                >Download CV <HiDownload className=' opacity-70 group-hover:translate-y-1 transition' /> {" "}</a>  
+                <a
+                className=' bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition border border-black/10 hover:text-gray-950' 
+                href='https://linkedin.com/in/emma-cameron-范恩茗）-54b671192' target='_blank'
+                ><BsLinkedin /></a>
+                <a
+                className=' bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition border border-black/10 hover:text-gray-950' 
+                href='https://github.com/Chamshron'
+                target='_blank'
+                ><FaGithubSquare /></a>  
+
+            </motion.div>
     </section>
   )
 }
