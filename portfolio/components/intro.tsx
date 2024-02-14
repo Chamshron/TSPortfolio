217c1eb4d810e2b89fdb95e8897b59c2f936a9dd
Chamsron <emmacameron219@gmail.com>
2024-02-14 11:36:10 +0000

    added animation to profile image

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -1,22 +1,42 @@
-"use client"
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import {motion} from "framer-motion";
 
 export default function Intro() {
   return (
-    <section className=' top-10'>
+    <section className=''>
         <div className='flex items-center justify-center'>
-            <div className=' max-w-40'>
+            <motion.div
+            initial={{ scale:0, opacity: 0}}
+            animate={{scale:1, opacity: 1}}
+            transition={{
+                type: "tween",
+                duration: 0.2,
+            }}
+            className='relative'
+            >
                 <Image src="/emmacameron.png"
                         width="250"
                         height="250"
                         alt='Portrait images of Emma Cameron'
                         quality="95"
                         priority={true}
-                        className=' h-24 w-24 rounded-full border-[0.35rem] border-white object-cover' />
-            </div>
+                        className=' h-30 w-30 rounded-full border-[0.35rem] border-white object-cover' />
+                        <motion.span className=' absolute bottom-5 right-1 text-5xl'
+                        initial={{opacity:0, scale:0}}
+                        animate={{opacity:1, scale: 1}}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 125,
+                            delay: 0.1,
+                            duration: 0.2,
+                        }}
+                        >
+                        🐯     
+                        </motion.span>
+            </motion.div>
         </div>
-
     </section>
   )
 }
