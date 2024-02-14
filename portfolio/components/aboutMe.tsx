eba2a12654e3a5edbc2a9bd7847bd57ee95dfa6d
Chamsron <emmacameron219@gmail.com>
2024-02-14 14:25:48 +0000

    minor asethetic changes

--- portfolio/components/aboutMe.tsx
+++ portfolio/components/aboutMe.tsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
     animate={{opacity:1, y:0}}
     transition={{delay: 0.175}}
     >
-        <h2 className=' font-bold text-2xl mb-3'>About Me</h2>
+        <h2 className=' font-medium text-3xl mb-3'>About Me</h2>
         <p className=' mb-3 text-justify'>After graduating in the pandemic with a degree in <span className=' font-medium'>Mandarin Chinese and German</span>, I persued a career in CRM Customer Support. It was there working closely with DevOps engineers I developed a passion for programming and enrolled in CodeClan, a coding bootcamp in Scotland. There I learned <span className='font-medium'>full-stack web development</span> and my passion for <span className=' italic'>problem-solving</span> blossomed. My core stack is <span className='font-medium'>React, Next.js, Node.js, and MongoDB</span>. I am also familar with Typescript, Python, and Java. I am always looking to learn new technologies.I am current looking for a <span className='font-medium'>full-time position as a software developer</span>.</p>
         <p className=' text-justify'>
             <span className='italic'>When {`I'm`} not coding</span>, I practice and coach a Japanese martial art called Kendo. I also enjoy <span className='font-medium'>learning new languages</span>, coding and non-coding langauges.</p>
