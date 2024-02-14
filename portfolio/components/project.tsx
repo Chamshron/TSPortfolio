bb3ddd11cd56c23929d4bd8242901b03fe7437e9
Chamsron <emmacameron219@gmail.com>
2024-02-14 15:14:56 +0000

    css styling

--- portfolio/components/project.tsx
+++ portfolio/components/project.tsx
@@ -16,9 +16,9 @@ export default function Project({
   
     return (
     <motion.div
-    className='group mb-3 sm:mb-8 last:mb-0'
+    className='group mb-28 sm:mb-8 last:mb-0'
     >
-        <section className=' bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8'>
+        <section className=' bg-indigo-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-indigo-200 transition sm:group-even:pl-8'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]'>
                 <a className='text-2xl font-semibold'
                 href={githubURL}
