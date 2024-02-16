79e7d42ccf52b3cf08e2fa908c9c6d0416faed2a
Chamsron <emmacameron219@gmail.com>
2024-02-16 15:48:57 +0000

    fixing contact me button so it redirects and changes active section

--- portfolio/components/contact.tsx
+++ portfolio/components/contact.tsx
@@ -2,26 +2,40 @@
 import React from 'react';
 import { useSectionInView } from '@/lib/hooks';
 import { FaPaperPlane } from 'react-icons/fa';
+import {motion} from "framer-motion";
 
 export default function Contact() {
     const {ref} = useSectionInView("Contact");
   return (
-    <section className='scroll-mt-28 mb-28 sm:mb-28 w-[min(100%,38rem)] text-center'
-    id='expericontactence'
-    ref={ref}>
+    <motion.section className='scroll-mt-28 mb-28 sm:mb-28 w-[min(100%,38rem)] text-center'
+    id='contact'
+    ref={ref}
+    initial={{
+        opacity:0,
+    }}
+    whileInView={{
+        opacity:1,
+    }}
+    transition={{
+        duration:1,
+    }}
+    viewport={{
+        once:true,
+    }}
+    >
         <h2 className='font-medium text-3xl mb-3 text-center'>Contact Me</h2>
         <p className=' text-gray-700'>Please contact me directly at <a className='underline' href='mailto:emmacameron219@gmail.com'>emmacameron219@gmail.com</a>  or through this form.</p>
         <form className=' flex flex-col mt-10'>
             <input className=' px-4 h-14 rounded-lg border border-black/10' 
             type='email'
             placeholder='Your Email Here' />
-            <textarea className=' px-4 h-50 my-3 p-4 rounded-lg border border-black/10'
+            <textarea className=' px-4 h-52 my-3 p-4 rounded-lg border border-black/10'
             placeholder='Your Message '
              />
             <button className=' group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-allfocus:scale-110 hover:scale-110 active:scale-105' 
             type='submit'
             >Submit<FaPaperPlane className=' text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 '/></button>
         </form>
-    </section>
+    </motion.section>
   )
 }
--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -7,9 +7,11 @@ import {BsArrowRight, BsLinkedin} from "react-icons/bs";
 import {HiDownload} from "react-icons/hi";
 import {FaGithubSquare} from "react-icons/fa";
 import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Intro() {
 const {ref} = useSectionInView("Home");
+const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
     
   return (
     <section 
@@ -65,7 +67,11 @@ const {ref} = useSectionInView("Home");
             }}
             >
                 <Link href="#contact"
-                className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:bg-950 active:scale-105 transition'>
+                className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:bg-950 active:scale-105 transition'
+                onClick={() => {
+                    setActiveSection("Contact")
+                    setTimeOfLastClick(Date.now());
+                }}>
                     Contact me here<BsArrowRight className=' opacity-70 group-hover:translate-x-1 transition' />{" "}</Link>
                 <a className=' group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10'
                 href='/EmmaCameron_Resume.pdf'
