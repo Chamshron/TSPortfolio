036a6a10ddda4906b8a0095cf3f8e94f4e730ab5
Chamsron <emmacameron219@gmail.com>
2024-02-16 14:31:24 +0000

    installed vertical timeline library and have added some experience data to data file

--- portfolio/components/experience.tsx
+++ portfolio/components/experience.tsx
@@ -1,15 +1,20 @@
-"use client"
-import React from 'react';
-import { useSectionInView } from '@/lib/hooks';
+"use client";
+import React from "react";
+import {VerticalTimeline, VerticalTimelineElement,} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
-    const {ref} = useSectionInView("Skills");
+    const {ref} = useSectionInView("Experience");
 
   return (
     <section className=' scroll-mt-28 mb-28 max-w-[53rem] text-center sm:mb-40'
     id='experience'
     ref={ref}>
-        <h2 className='font-medium text-3xl mb-3'>Experience</h2>
+        <h2 className='font-medium text-3xl mb-3'>My Experience</h2>
+
+
     </section>
     
   )
--- portfolio/lib/data.ts
+++ portfolio/lib/data.ts
@@ -1,6 +1,9 @@
 import React from "react";
 import subwaySamurai from "@/public/SubwaySamurai.png";
 import navigate from "@/public/Navigate.png";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
 
 export const links = [
     {
@@ -65,3 +68,30 @@ export const links = [
     "Jester",
     "Mocha"
  ] as const;
+
+ export const experiencesData = [
+    {
+      title: "Graduated CodeClan",
+      location: "Glasgow",
+      description:
+        "I graduated after 16 weeks of intensive studying. I completed over 800 hours worth of hands-on coding, through both solo and group projects.",
+      icon: React.createElement(LuGraduationCap),
+      date: "2019",
+    },
+    {
+      title: "Customer Support Staff",
+      location: "Blackbaud Europe",
+      description:
+        "Since joining Blackbaud Europe in 2022, I quickly progress from an Associate to Staff member, supporting multiple different products including CRMs. Due to this I received the We Expect the Best award in Q2 of 2022.",
+      icon: React.createElement(CgWorkAlt),
+      date: "April 2022 - Jul 2023",
+    },
+    {
+      title: "Customer Support Associate Specalist",
+      location: "Blackbaud",
+      description:
+        "After joining Blackbaud in 2021, I went from a Customer Support Associate to an Associate Specialist for the North American region supporting the CRM the Raiser’s Edge/RE NXT. During my time I had many responsibilities, below are my most recent responsibilities. .",
+      icon: React.createElement(FaReact),
+      date: "April 2021 - April 2022",
+    },
+  ] as const;
