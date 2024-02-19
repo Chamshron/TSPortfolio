e6ecf9ba4d24c2691bf997fbe3440f3b4e56d66b
Chamsron <emmacameron219@gmail.com>
2024-02-19 11:53:31 +0000

    server side validation

--- portfolio/actions/sendEmail.ts
+++ portfolio/actions/sendEmail.ts
@@ -1,24 +1,30 @@
 "use server";
 
+import { motionValue } from "framer-motion";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const validateString = (value: unknown) => {
+    if (!value || typeof motionValue !== "string"){
+        return false;
+    }
+    return true;
+}
 
 export const sendEmail = async (formData : FormData) => {
 
     const message = formData.get('senderMessage');
     const email = formData.get('senderEmail');
 
-    if (!message || typeof message !== "string"){
+    if (validateString(email)){
         return{
-            error: "Woops! That's an invalid message friend"
-        };
+            error: "Woops! invalid email friend"
+        }
     }
-
-    if (!email || typeof email !== "string"){
+    if(validateString(message)){
         return{
-            error: "Woops! That's an invalid email address friend"
-        };
+            error: "Woops! Invalid message friend"
+        }
     }
 
     resend.emails.send({
