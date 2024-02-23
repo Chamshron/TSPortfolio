97ec76fc22f055ac40fbeb1389d64848618ac935
Chamsron <emmacameron219@gmail.com>
2024-02-23 13:43:43 +0000

    made submit button it's own component for pending mechanic and animation

--- portfolio/components/contact.tsx
+++ portfolio/components/contact.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from 'react';
 import { useSectionInView } from '@/lib/hooks';
-import { FaPaperPlane } from 'react-icons/fa';
 import {motion} from "framer-motion";
 import {sendEmail} from "@/actions/sendEmail";
+import SubmitButton from './submitButton';
 
 export default function Contact() {
     const {ref} = useSectionInView("Contact");
+    
 
   return (
     <motion.section className='scroll-mt-28 mb-28 sm:mb-28 w-[min(100%,38rem)] text-center'
@@ -48,11 +49,7 @@ export default function Contact() {
             maxLength={5000}
             />
 
-            <button className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-allfocus:scale-110 hover:scale-110 active:scale-105' 
-            type='submit'
-            >
-            Submit
-            <FaPaperPlane className=' text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 '/></button>
+            <div className=' flex justify-center'><SubmitButton /></div>
         </form>
     </motion.section>
   )
