2964275870cd97c9ec8e349d10ac682127488b0c
Chamsron <emmacameron219@gmail.com>
2024-02-23 13:58:54 +0000

    styling footer

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
         <Footer />
         <Toaster position='top-right'/>
 
-        <div className="bg-violet-100 absolute bottom-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-        <div className="bg-sky-100 absolute bottom-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-violet-100 absolute bottom-[6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-sky-100 absolute bottom-[1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         </body>
     </html>
   )
--- portfolio/components/footer.tsx
+++ portfolio/components/footer.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
-        <small>
+    <footer className='mb-10 px-4 text-center text-gray-500 '>
+        <small className='block mb-2 text-xs'>
             &copy; 2024 Emma Cameron. All rights reserved.
         </small>
-        <p>
-            <span>About this website: </span> built with React & Next,js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email and Resend, Vercel hosting.
+        <p className=' text-xs '>
+            <span className=' font-semibold'>About this website: </span> built with React & Next,js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email and Resend, Vercel hosting.
         </p>
     </footer>
   )
