0d45e5fce0435c36a86df56f6aff9f884e392e8d
Chamsron <emmacameron219@gmail.com>
2024-02-26 14:27:29 +0000

    updated the styling for dark mode on experiences

--- portfolio/components/experience.tsx
+++ portfolio/components/experience.tsx
@@ -33,18 +33,18 @@ export default function Experience() {
                         padding: "1.3rem 2rem",
                     }}
                     contentArrowStyle={{
-                        borderRight:"0.4rem solid #9ca3af",
+                        borderRight:theme === 'light' ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255, 255, 255, 0.5)",
                     }}
                     date={item.date}
                     icon={item.icon}
                     iconStyle={{
-                        background: "white",
+                        background: theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                         fontSize: "1.5rem"
                     }}
                     >
                         <h3 className=" font-semibold capitalize">{item.title}</h3>
-                        <p className=" font-normal !mt-0">{item.location}</p>
-                        <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
+                        <p className=" font-normal !mt-0 dark:text-white/80">{item.location}</p>
+                        <p className="!mt-1 !font-normal text-gray-700 dark:text-white/70">{item.description}</p>
                     </VerticalTimelineElement>
             ))}
       </VerticalTimeline>
