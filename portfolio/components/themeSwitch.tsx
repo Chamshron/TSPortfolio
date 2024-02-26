825aac8affae3ab9645b9148f9fbf5459be03726
Chamsron <emmacameron219@gmail.com>
2024-02-26 13:40:07 +0000

    fixing tailwind

--- portfolio/components/themeSwitch.tsx
+++ portfolio/components/themeSwitch.tsx
@@ -11,9 +11,11 @@ export default function ThemeSwitch() {
         if(theme === "light"){
             setTheme("dark");
             window.localStorage.setItem("theme", "dark");
+            document.documentElement.classList.add("dark");
         } else {
             setTheme("light");
             window.localStorage.setItem("theme", "light");
+            document.documentElement.classList.remove("dark");
         }
     };
 
@@ -21,8 +23,12 @@ export default function ThemeSwitch() {
         const localTheme = window.localStorage.getItem("theme") as Theme | null;
         if(localTheme){
             setTheme(localTheme);
+            if (localTheme === "dark"){
+                document.documentElement.classList.add("dark");
+            }
         } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             setTheme("dark");
+            document.documentElement.classList.add("dark");
         }
     }, []);
 
