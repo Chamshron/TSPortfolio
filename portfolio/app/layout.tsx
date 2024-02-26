14bd65493ed90b51c290c6da5ead71f18adcf95c
Chamsron <emmacameron219@gmail.com>
2024-02-26 13:29:56 +0000

    creating a light and dark mode button

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast';
 import Footer from '@/components/footer';
+import ThemeSwitch from '@/components/themeSwitch';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         </ActiveSectionContextProvider>
         <Footer />
         <Toaster position='top-right'/>
+        <ThemeSwitch />
 
         <div className="bg-violet-100 absolute bottom-[6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <div className="bg-sky-100 absolute bottom-[1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
