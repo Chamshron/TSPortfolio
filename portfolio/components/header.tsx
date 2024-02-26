fe784e71745fe56735cbeb82f24d0731e59d0429
Chamsron <emmacameron219@gmail.com>
2024-02-26 13:49:40 +0000

    added dark mode styling for header and layout background

--- portfolio/components/header.tsx
+++ portfolio/components/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     
   return(
     <header className="z-[999] relative">
-        <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0}}
         animate={{y: 0, x: "-50%", opacity: 1}}
         >
@@ -24,7 +24,8 @@ export default function Header() {
                     initial={{ y: -100, opacity: 0}}
                     animate={{y: 0, opacity: 1}}
                     key={link.hash}>
-                        <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-indigo-950 transition hover:font-bold', {"text-gray-950": activeSection === link.name})}
+                        <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-indigo-950 transition hover:font-bold dark:text-gray-500 dark:hover-gray-300',
+                         {"text-gray-950 dark:text-gray-200": activeSection === link.name})}
                         href={link.hash}
                         onClick={()=> {
                             setActiveSection(link.name)
@@ -32,7 +33,7 @@ export default function Header() {
                         }}
                         >
                         {link.name}
-                        {link.name === activeSection && (<motion.span className=' bg-gray-100 rounded-full absolute inset-0 -z-10'
+                        {link.name === activeSection && (<motion.span className=' bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-600'
                         layoutId='activeSection'
                         initial={{
                             type:"spring",
