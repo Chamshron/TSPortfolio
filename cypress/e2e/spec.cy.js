b65d695b61495ae51519853c04cabd5b69c6bf27
Chamsron <emmacameron219@gmail.com>
2024-03-17 19:53:06 +0000

    Contact me button redirect works

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
 describe('visit website', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
+    cy.contains('Contact me here').click()
+    cy.url().should('include','/#contact')
   })
 })
